b192d4f657a6ced88d67f36eb1b272c17b2f3115
crimsonwrench <garon312@gmail.com>
2019-03-18 17:13:33 +0300

    API route for fetching currency

---
+++ app/Http/Controllers/Api/CurrencyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    public function fetch($firstCurrency, $secondCurrency)
+    {
+
+
+    }
+}
